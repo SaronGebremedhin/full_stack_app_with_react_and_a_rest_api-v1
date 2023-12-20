0f4e6f912ddc9d17e76a55be6dbc8fd2922564c7
Kalkidan-Geb <144774340+Kalkidan-Geb@users.noreply.github.com>
2023-12-20 15:34:16 -0800

    Update ErrorsDisplay.js

--- client/src/components/ErrorsDisplay.js
+++ client/src/components/ErrorsDisplay.js
@@ -1,16 +1,18 @@
-import React from 'react';
-
 const ErrorsDisplay = ({ errors }) => {
-  return (
-    <div className="validation-errors">
-      <h2>Validation Errors</h2>
-      <ul>
-        {errors.map((error, index) => (
-          <li key={index}>{error}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (errors.length) {
+    return (
+      <div className="validation-errors">
+        <h2>Validation Errors</h2>
+        <ul>
+          {errors.map((error, index) => (
+            <li key={index}>{error}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default ErrorsDisplay;
