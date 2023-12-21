61585640373d9146108d2e34c57d566cfb79646d
Kalkidan-Geb <144774340+Kalkidan-Geb@users.noreply.github.com>
2023-12-20 17:15:30 -0800

    Update UserContext.js

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-import { api } from '../utilities/apiHelper';
+// import { api } from '../utilities/apiHelper';
 
 const UserContext = createContext();
 
