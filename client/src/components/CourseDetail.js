f5894463e2b883011bff5ca62a317e98ad2a4545
Saron Worku <saronworku@Sarons-MacBook-Air.local>
2023-12-21 02:01:07 -0800

    updated compnents

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -3,65 +3,58 @@ import ReactMarkdown from 'react-markdown';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { api } from '../utilities/apiHelper';
 import { useUser } from '../context/UserContext';
-// import ErrorsDisplay from './ErrorsDisplay.js';
 
-
-function CourseDetail () {
+function CourseDetail() {
   const { authUser: user } = useUser();
   const { id } = useParams();
   const navigate = useNavigate();
 
   // State for storing course details and validation errors
   const [course, setCourse] = useState({});
-  // const [errors, setErrors] = useState([]);
 
   // Effect hook to fetch the course details on component mount
   useEffect(() => {
     const fetchCourse = async () => {
-        try {
-            const res = await api(`/courses/${id}`, 'GET')
-            const json = await res.json();
-            if (res.status === 200) {
-                setCourse(json);
-            } else if (res.status === 404) {
-                navigate('/notfound');
-            } else {
-                throw new Error();
-            }
-        } catch (error) {
-            console.log("Error fetching and parsing the data", error)
-            navigate('/error');
+      try {
+        const res = await api(`/courses/${id}`, 'GET');
+        const json = await res.json();
+        if (res.status === 200) {
+          setCourse(json);
+        } else if (res.status === 404) {
+          navigate('/notfound');
+        } else {
+          throw new Error();
         }
-    }
+      } catch (error) {
+        console.log("Error fetching and parsing the data", error);
+        navigate('/error');
+      }
+    };
     fetchCourse();
-}, [id, navigate])
-
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
- 
+  }, [id, navigate]);
 
   // Function to handle course deletion
   const handleDelete = async () => {
     try {
+      console.log('Deleting course...');
       const response = await fetch(`http://localhost:5001/api/courses/${id}`, {
         method: 'DELETE',
         headers: {
+          'Content-Type': 'application/json',
           Authorization: `Basic ${btoa(`${user.emailAddress}:${user.password}`)}`,
         },
       });
 
-      // If the deletion is successful, navigate to the homepage
       if (response.status === 204) {
+        console.log('Course deleted successfully');
+        // Redirect to the home page or another appropriate page after deletion
         navigate('/');
       } else if (response.status === 403) {
-        // If user is not authorized to delete, navigate to forbidden route
-        navigate('/forbidden');
-      } else if (response.status === 404) {
-        // If the course is not found, navigate to notfound route
-        navigate('/notfound');
+        console.log('User not authorized to delete the course');
+        // Handle forbidden access if needed
       } else {
-        // If an unexpected error occurs, navigate to the error route
-        navigate('/error');
+        console.log('Unexpected error occurred while deleting course');
+        // Handle other status codes if needed
       }
     } catch (error) {
       console.error('Error deleting course:', error);
@@ -69,50 +62,70 @@ function CourseDetail () {
   };
 
   return (
-    <main>
-    <div className="actions--bar">
-        <div className="wrap">
-            {}
-            {
-                user && user?.id === course?.user?.id ? (
-                    <>
-                        <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
-                        <button className="button" onClick={handleDelete}>Delete Course</button>
-                    </>
-                ) : (<></>)
-            }
-            <Link className="button button-secondary" to='/'>Return to List</Link>
+    <div>
+      <div className="actions--bar">
+        <div className="bounds">
+          <div className="grid-100">
+            <span>
+              <Link className="button" to={`/courses/${id}/update`}>
+                Update Course
+              </Link>
+              <button className="button" onClick={handleDelete}>
+                Delete Course
+              </button>
+            </span>
+            <Link className="button button-secondary" to="/">
+              Return to List
+            </Link>
+          </div>
         </div>
-    </div>
-
-    <div className="wrap">
-        <h2>Course Detail</h2>
-        <form>
-            <div className="main--flex">
-                <div>
-                    <h3 className="course--detail--title">Course</h3>
-                    <h4 className="course--name">{course.title}</h4>
-                    <p>{course.User ? `${course.User.firstName} ${course.User.lastName}` : 'Unknown User'}</p>
-
-                    <ReactMarkdown>{course.description}</ReactMarkdown>
-
-                </div>
-                <div>
-                    <h3 className="course--detail--title">Estimated Time</h3>
-                    <p>{course.estimatedTime || 'Ask instructor'}</p>
-
-                    <h3 className="course--detail--title">Materials Needed</h3>
-                    <ul className="course--detail--list">
-
-                        <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
-
-                    </ul>
-                </div>
+      </div>
+      <div className="bounds course--detail">
+        <div className="grid-66">
+          <div className="course--header">
+            <h2 className="course--title-purple">{course.title}</h2>
+            <p className="course--author">
+              By {course.User ? `${course.User.firstName} ${course.User.lastName}` : 'Unknown User'}
+            </p>
+            <h4 className="course--label">Course Details</h4>
+            <div className="course--description">
+              <ReactMarkdown>{course.description}</ReactMarkdown>
             </div>
-        </form>
+          </div>
+        </div>
+        <div className="grid-25 grid-right">
+          <div className="course--stats">
+            <ul className="course--stats--list">
+              <li className="course--stats--list--item">
+                <h3>Estimated Time</h3>
+                <hr className="course--line" />
+                <p className="course--time">{course.estimatedTime || 'N/A'}</p>
+              </li>
+              <li className="course--stats--list--item">
+                <h4>Materials Needed</h4>
+                <hr className="course--line" />
+                <ul className="course--materials">
+                  {course.materialsNeeded
+                    ? course.materialsNeeded.split('\n').map((item, index) => (
+                        <li key={index}>{item}</li>
+                      ))
+                    : 'N/A'}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-</main>
-)
+  );
 }
 
-export default CourseDetail
+export default CourseDetail;
+
+
+
+
+
+
+
+
--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-
 import ErrorsDisplay from "./ErrorsDisplay.js";
 import { useUser } from "../context/UserContext.js";
+import Forbidden from "./Forbidden.js";
 
 const UpdateCourse = () => {
   const { authUser: user } = useUser();
   const { id } = useParams();
   const navigate = useNavigate();
 
-  // State for storing form data and validation errors
   const [formData, setFormData] = useState({
     title: '',
     description: '',
@@ -17,20 +16,19 @@ const UpdateCourse = () => {
     materialsNeeded: '',
   });
   const [errors, setErrors] = useState([]);
+  const [isUserAuthorized, setUserAuthorized] = useState(true); // Add state to track user authorization
 
-  // Effect hook to fetch the course details on component mount
   useEffect(() => {
     const fetchCourse = async () => {
       try {
         const response = await fetch(`http://localhost:5001/api/courses/${id}`);
         const data = await response.json();
 
-        // If course is not found, navigate to notfound route
         if (response.status === 404) {
           navigate('/notfound');
         } else if (user.id !== data.userId) {
-          // If the current user is not the owner, navigate to forbidden route
-          navigate('/forbidden');
+          // Set user authorization state to false if the user is not the owner
+          setUserAuthorized(false);
         } else {
           setFormData(data);
         }
@@ -40,16 +38,12 @@ const UpdateCourse = () => {
     };
 
     fetchCourse();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id, user, navigate]);
 
-  // Function to handle form data changes
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -64,17 +58,13 @@ const UpdateCourse = () => {
       });
 
       if (response.status === 204) {
-        // If course update is successful, navigate to the course details
         navigate(`/courses/${id}`);
       } else if (response.status === 400) {
-        // If there are validation errors, set the errors state
         const data = await response.json();
         setErrors(data.errors);
       } else if (response.status === 403) {
-        // If user is not authorized, navigate to forbidden route
         navigate('/forbidden');
       } else {
-        // If an unexpected error occurs, navigate to the error route
         navigate('/error');
       }
     } catch (error) {
@@ -82,88 +72,94 @@ const UpdateCourse = () => {
     }
   };
 
-  // Function to handle cancel button click
   const handleCancel = () => {
     navigate(`/courses/${id}`);
   };
 
   return (
-      <div className="bounds course--detail">
+    <div className="bounds course--detail">
+      {!isUserAuthorized ? ( // Conditionally render Forbidden component if user is not authorized
+        <Forbidden />
+      ) : (
+        <>
           <h1>Update Course</h1>
           <ErrorsDisplay errors={errors} />
           <div>
-              <form onSubmit={handleSubmit}>
-                  <div className="grid-66">
-                      <div className="course--header">
-                          <h4 className="course--label">Course</h4>
-                          <div>
-                              <label htmlFor="title">Course Title</label>
-                              <input
-                                  id="title"
-                                  name="title"
-                                  type="text"
-                                  placeholder="Course title..."
-                                  value={formData.title}
-                                  onChange={handleChange}
-                              />
-                          </div>
-                          <p>By {formData.User ? `${formData.User.firstName} ${formData.User.lastName}` : "Unknown User"}</p>
-                      </div>
-                      <div className="course--description">
-                          <div>
-                              <label htmlFor="description">Course Description</label>
-                              <textarea
-                                  id="description"
-                                  name="description"
-                                  placeholder="Course description..."
-                                  value={formData.description}
-                                  onChange={handleChange}
-                              />
-                          </div>
-                      </div>
+            <form onSubmit={handleSubmit}>
+              <div className="grid-66">
+                <div className="course--header">
+                  <h4 className="course--label">Course</h4>
+                  <div>
+                    <input
+                      id="title"
+                      name="title"
+                      type="text"
+                      placeholder="Course title..."
+                      value={formData.title}
+                      onChange={handleChange}
+                    />
                   </div>
-                  <div className="grid-25 grid-right">
-                      <div className="course--stats">
-                          <ul className="course--stats--list">
-                              <li className="course--stats--list--item">
-                                  <h4>Estimated Time</h4>
-                                  <div>
-                                      <input
-                                          id="estimatedTime"
-                                          name="estimatedTime"
-                                          type="text"
-                                          placeholder="Hours"
-                                          value={formData.estimatedTime}
-                                          onChange={handleChange}
-                                      />
-                                  </div>
-                              </li>
-                              <li className="course--stats--list--item">
-                                  <h4>Materials Needed</h4>
-                                  <div>
-                                      <textarea
-                                          id="materialsNeeded"
-                                          name="materialsNeeded"
-                                          placeholder="List materials..."
-                                          value={formData.materialsNeeded}
-                                          onChange={handleChange}
-                                      />
-                                  </div>
-                              </li>
-                          </ul>
+                  <p>
+                    By: {formData.User ? `${formData.User.firstName} ${formData.User.lastName}` : "Unknown User"}
+                  </p>
+                </div>
+                <div className="course--description">
+                  <div>
+                    <textarea
+                      id="description"
+                      name="description"
+                      placeholder="Course description..."
+                      value={formData.description}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="grid-25 grid-right">
+                <div className="course--stats">
+                  <ul className="course--stats--list">
+                    <li className="course--stats--list--item">
+                      <h4>Estimated Time</h4>
+                      <div>
+                        <input
+                          id="estimatedTime"
+                          name="estimatedTime"
+                          type="text"
+                          placeholder="Hours"
+                          value={formData.estimatedTime}
+                          onChange={handleChange}
+                        />
                       </div>
-                  </div>
-                  <div className="grid-100 pad-bottom">
-                      <button className="button" type="submit">
-                          Update Course
-                      </button>
-                      <button className="button button-secondary" onClick={handleCancel}>
-                          Cancel
-                      </button>
-                  </div>
-              </form>
+                    </li>
+                    <li className="course--stats--list--item">
+                      <h4>Materials Needed</h4>
+                      <div>
+                        <textarea
+                          id="materialsNeeded"
+                          name="materialsNeeded"
+                          placeholder="List materials..."
+                          value={formData.materialsNeeded}
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="grid-100 pad-bottom">
+                <button className="button" type="submit">
+                  Update Course
+                </button>
+                <button className="button button-secondary" onClick={handleCancel}>
+                  Cancel
+                </button>
+              </div>
+            </form>
           </div>
-      </div>
+        </>
+      )}
+    </div>
   );
-  }
+};
+
 export default UpdateCourse;
